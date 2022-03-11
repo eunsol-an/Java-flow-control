c50aaf7be18b68f709a7b04b21ae18cfa2ef7836
eunsol-an <ae__sol@naver.com>
2022-03-11 22:14:15 +0900

    if문 응용(아이디 인증+비밀번호 인증)
    아이디 인증에 이어 비밀번호까지 인증하는 프로그램을 1)if문 중첩 방식과 2)논리 연산자 사용 방식으로 구현

--- src/AuthApp.java
+++ src/AuthApp.java
@@ -2,17 +2,29 @@
 public class AuthApp {
 
 	public static void main(String[] args) {
-		
-		System.out.println(args[0]);
 		// if문 응용
 		
 		String id = "oneul";
 		String inputId = args[0];
 		
+		String pass = "1234";
+		String inputPass = args[1];
+		
 		System.out.println("Hi.");
 		
-		//if(inputId == id) {
+		// if문 중첩해서 사용
 		if(inputId.equals(id)) {
+			if(inputPass.equals(pass)) {
+				System.out.println("Master!");
+			} else {
+				System.out.println("Wrong Password!");
+			}
+		} else {
+			System.out.println("Who are you?");
+		}
+		
+		// 논리 연산자 && 사용
+		if(inputId.equals(id) && inputPass.equals(pass)) {
 			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
